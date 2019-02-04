01378774d34180b392957e3b2679a2d0d55358e0
Jonas Tranberg <jonas_ts@msn.com>
2019-02-04 01:44:50 +0100

    fixed missing systemd service enabling

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 sudo cp stalenhag.py /usr/local/bin/stalenhag
 sudo cp -p systemd/* /etc/systemd/user
-systemctl enable --user stalenhag.timer
-systemctl start --user stalenhag.timer
+systemctl enable --user stalenhag.service stalenhag.timer
+systemctl start --user stalenhag.service stalenhag.timer
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-systemctl stop --user stalenhag.timer
-systemctl disable --user stalenhag.timer
+systemctl stop --user stalenhag.service stalenhag.timer
+systemctl disable --user stalenhag.service stalenhag.timer
 sudo rm /usr/local/bin/stalenhag
 sudo rm /etc/systemd/user/stalenhag.*
