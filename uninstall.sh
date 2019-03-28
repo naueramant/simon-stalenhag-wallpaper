48b1d780ca859807ee9d076bb38ab82f7f5434f8
arcuo <benjamin.zachariae@gmail.com>
2019-03-28 12:06:51 +0100

    updated with favorites and helper

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
+echo Uninstalling Stålenhag Wallpaper
 systemctl stop --user stalenhag.service stalenhag.timer
 systemctl disable --user stalenhag.service stalenhag.timer
 sudo rm /usr/local/bin/stalenhag
 sudo rm /etc/systemd/user/stalenhag.*
+sudo rm -rf ~/.stalenhag/
+sudo rm -rf ~/Pictures/Stålenhag/
